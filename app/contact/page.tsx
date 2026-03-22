8536a5e604b5bc4210d31b7bfa8dbabce58aef0e
LeebaN <nabeel.khan3009@gmail.com>
2026-03-23 07:57:36 +0800

    initial commit

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Contact</h1>
+      <p>Email: hello@leeban.co</p>
+    </main>
+  )
+}
---
+++ app/privacy/page.tsx
@@ -0,0 +1,8 @@
+export default function Privacy() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Privacy Policy</h1>
+      <p>We respect your privacy.</p>
+    </main>
+  )
+}
